a4d1ef84559926119a3c3fed05ad6dd5a6934b2a
Arushi Gupta <67069824+arushi2103@users.noreply.github.com>
2025-06-11 15:55:56 +0530

    Time: 3 ms (41.1%), Space: 41.9 MB (31.8%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s==null || s.length() <=1) return false;
+        Stack<Character> stack= new Stack<>();
+        Map<Character,Character> hmap= Map.of(
+             ')', '(', 
+            '}', '{', 
+            ']', '['
+        );
+
+        for(char ch : s.toCharArray()){
+            if( hmap.containsKey(ch)){
+                // it is a closing bracket - 
+                // check is stack is empty return false 
+                // if top element is not the same opening as closing 
+                if (stack.isEmpty() || stack.peek() != hmap.get(ch)){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }else{
+                //it is an opening bracket - push to stack
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty(); 
+
+
+
+        //------------------------------------------
+        
+        
+    }
+}
