595159a995ae13751e2311950fdf0a36723a47eb
Arushi Gupta <67069824+arushi2103@users.noreply.github.com>
2025-06-11 16:40:03 +0530

    Time: 2 ms (89.11%), Space: 56.1 MB (27.46%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,15 +4,18 @@ class Solution {
         int[] ans = new int[n];
         int product=1;
         
+        //contains the product of all elements to the left of index i
         int[] pre= new int[n];
-        pre[0]=1;
-        //create prefix product array - except current index
+        pre[0]=1; // nothing is in left 
         for(int i=1; i<n; i++)pre[i] = pre[i-1]*nums[i-1];
+
+        //contains the product of all elements to the right of index i
         int[] suf= new int[nums.length];
-        suf[n-1]=1;
-        //create the suffix product array - except the current index
+        suf[n-1]=1; // nothing is in right 
         for(int i= n-2; i>=0; i--)suf[i]=suf[i+1]*nums[i+1];
 
+
+        // product of all the elements from left and right
         for(int i=0; i<n; i++){
             ans[i] = pre[i] * suf[i];
         }
