49162bb3816762cd2e95a99a87ae96967a536dc7
Arushi Gupta <67069824+arushi2103@users.noreply.github.com>
2025-06-11 16:07:19 +0530

    Time: 2 ms (97.22%), Space: 41.7 MB (81.56%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,28 +2,28 @@ class Solution {
     public boolean isValid(String s) {
         if(s==null || s.length() <=1) return false;
         Stack<Character> stack= new Stack<>();
-        Map<Character,Character> hmap= Map.of(
-             ')', '(', 
-            '}', '{', 
-            ']', '['
-        );
+        
 
         for(char ch : s.toCharArray()){
-            if( hmap.containsKey(ch)){
-                // it is a closing bracket - 
-                // check is stack is empty return false 
-                // if top element is not the same opening as closing 
-                if (stack.isEmpty() || stack.peek() != hmap.get(ch)){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty())return false;
+
+                if(ch == ')' && stack.peek() != '(') {
+                return false;
+                }else if( ch == '}' && stack.peek() != '{'){
+                    return false;
+                }else if (ch == ']' && stack.peek() != '[') {
                     return false;
                 }else{
+                    // if it is a valid parenthesis at the top - remove topmost character 
                     stack.pop();
                 }
-            }else{
-                //it is an opening bracket - push to stack
-                stack.push(ch);
             }
         }
-        return stack.isEmpty(); 
+        // stack should be empty 
+        return stack.isEmpty();
 
 
 
