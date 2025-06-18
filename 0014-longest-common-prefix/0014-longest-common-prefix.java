25fd8c3e681cd2fba45a3c788f9e803617e09154
Arushi Gupta <67069824+arushi2103@users.noreply.github.com>
2025-06-19 04:08:24 +0530

    Time: 0 ms (100%), Space: 41.2 MB (97.98%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,19 +1,32 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        //tale 1st string as prefix- now compare each letter with next string and - substring till it matched 
-        if(strs==null || strs.length==0)return "";
+        if(strs==null || strs.length ==0)return "";
 
         String prefix=strs[0];
-        for(int i=1;i<strs.length;i++){
-            int j=0;
-            //check index bounds first then compare characters
-            while(j<prefix.length() && j<strs[i].length() && prefix.charAt(j)==strs[i].charAt(j) ){
-                j++;
+        for(int i=0;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                //trim thr prefix from end till it contains the common prefix only 
+                prefix= prefix.substring(0,prefix.length()-1);
             }
-                prefix=prefix.substring(0,j);
-                //early check if prefix is empty-no common prefix
-                if(prefix.isEmpty())return "";
         }
         return prefix;
+        
     }
 }
+
+
+// take 1st string as prefix- now compare each letter with next string and - substring till it matched 
+//         if(strs==null || strs.length==0)return "";
+
+//         String prefix=strs[0];
+//         for(int i=1;i<strs.length;i++){
+//             int j=0;
+//             //check index bounds first then compare characters
+//             while(j<prefix.length() && j<strs[i].length() && prefix.charAt(j)==strs[i].charAt(j) ){
+//                 j++;
+//             }
+//                 prefix=prefix.substring(0,j);
+//                 //early check if prefix is empty-no common prefix
+//                 if(prefix.isEmpty())return "";
+//         }
+//         return prefix;
